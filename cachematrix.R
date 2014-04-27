091125d0831ec37f2a8fd081668ac3b96d3ed7b8
allene <allene.bhasker@gmail.com>
2014-04-27 23:38:05 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##
+##  Matrix inversion is usually a costly computation these following two utility functions
+##  can be used to cache a matrix and its inverse. 
+##
+##
+##  makeCacheMatrix: This function creates a special "matrix" object that can store 
+##  information about its inverse.
+##
 
 makeCacheMatrix <- function(x = matrix()) {
+       #for first time set matrix to NULL
+       invMtrix <- NULL
+	   #store original matrix 
+       set <- function( y ) {
+			#set global value	
+              x <<- y
+			#clear 	
+              invMtrix <<- NULL
+       }
+	   #get the value if yes get the value
+       get <- function() x
+	   #set inverse matrix
+       setinverse <- function(inverse) invMtrix <<- inverse
+	   #get inverse matrix
+       getinverse <- function() invMtrix
+       list(set = set, get = get,  setinverse = setinverse,  getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+##  cacheSolve: This function computes the inverse of the special "matrix" returned by
+## another utility function makeCacheMatrix . If the inverse has already been calculated
+## (and the matrix has not changed), then the cachesolve will retrieve the inverse from the cache.
+## It will not re calculate inverse.
+##  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       #get inverse
+	   invMtrix  <- x$getinverse()
+	   #check if inverse already exists
+       if(!is.null(invMtrix )) {
+			#no need to calculate again
+              message("Inverse caluculation skipped. returning previously cached inverse")
+              return(invMtrix )
+       }
+	   #for first time, calculate inverse
+       newInvMtrix <- x$get()
+       invMtrix <- solve(newInvMtrix)
+       x$setinverse(invMtrix)
+	   #return inverse
+       invMtrix
 }
